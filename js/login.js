36252bf39bf0d23f2239d1089e3bcd3e4d01b5cf
cilebritain <2023697797@qq.com>
2019-04-08 22:30:03 +0800

    4.8备份1
    注册&登录部分完工
    还剩图片放大&profile&购物车
    冲鸭！

--- js/login.js
+++ js/login.js
@@ -1,15 +1,63 @@
 var code;
 
-function login(){
-    
-    var user=document.getElementById("username").nodeValue;
-    var password=document.getElementById("user_password").nodeValue;
-    var vert=code.toLowercase();
-    var input=document.getElementById("vertification_input").nodeValue.toLowerCase();
-    var flag=false;
+function click_login(){
 
-    if(user.length==0){alert("请输入用户名");}
-    if(password.length==0){alert("请输入密码");}
-    if(input.length==0){alert("请输入验证码");}
-    
+    var user=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    var vert=code.toLowerCase();
+    var input=document.getElementById('vertification_input').value.toLowerCase();
+    var flag=false;
+    if(user.length<=0){alert("请输入用户名");}
+    else if(password.length==0){alert("请输入密码");}
+    else if(input.length==0){alert("请输入验证码");}
+    else if(user!="cilebritain123"){alert("该用户不存在");}
+    else if(password!="cilebritain"){alert("密码错误！");}
+    else if(vert!=input){
+        alert("验证码错误！");
+        change_ver();
+        document.getElementById('vertification_input').value="";
+    }
+    else {
+        alert("登录成功");
+        flag=true;
+    }
+    if(flag){
+        document.cookie="username=cilebritain123";
+        document.getElementById('logo_slogan').style.display='none';
+        document.getElementById('logo_slogan1').style.display='block';
+        document.getElementById('login_dialog').style.display='none';
+        document.getElementById('login2').style.display='none';
+    }
 }
+
+function login() {
+    document.getElementById('login_dialog').style.display='block';
+    document.getElementById('login2').style.display='block';
+}
+
+function logout() {
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 GMT ";
+    document.getElementById('logo_slogan').style.display='block';
+    document.getElementById('logo_slogan1').style.display='none';
+}
+
+function cancel_login() {
+    document.getElementById('login_dialog').style.display='none';
+    document.getElementById('login2').style.display='none';
+}
+
+function generate_code(){
+    var a="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    var b="";
+    for(var i=1;i<=4;i++){
+        var ind=Math.floor(Math.random()*62);
+        b+=a.charAt(ind);
+    }
+    return b;
+}
+
+function change_ver(){
+    code=generate_code();
+    document.getElementById('vertification_code').innerHTML=code;
+}
+
---
+++ js/register.js
@@ -0,0 +1,135 @@
+function click_register(){
+    
+    var name=document.getElementById('username2').value;
+    var password=document.getElementById('password2').value;
+    var check=document.getElementById('check_password').value;
+    var email=document.getElementById('email').value;
+    var succeed=false;
+
+    if(name.length==0){
+        alert("用户名不能为空");
+    }else if(password.length==0){
+        alert("密码不能为空");
+    }else if(password!=check){
+        alert("密码不相等");
+    }else if(email.length==0){
+        alert("邮件不能为空");
+    }else if(check_email(email)==false){
+        alert("邮件格式不正确");
+    }else if(check_password(password)==false){
+        alert("密码格式不正确");
+    }else if(check_username(name)==false){
+        alert("用户名格式不正确");
+    }else if(name==password){
+        alert("密码不能与用户名相同");
+    }else{
+        alert("注册成功");
+        succeed=true;
+    }
+
+    if(succeed){
+        document.cookie="username=cilebritain123";
+        document.getElementById('register_dialog').style.display='none';
+        document.getElementById('register2').style.display='none';
+        document.getElementById('logo_slogan').style.display='none';
+        document.getElementById('logo_slogan1').style.display='block';
+    }
+
+}
+
+function register(){
+    document.getElementById('register_dialog').style.display='block';
+    document.getElementById('register2').style.display='block';
+}
+
+function cancle_register(){
+    document.getElementById('register_dialog').style.display='none';
+    document.getElementById('register2').style.display='none';   
+}
+
+function check_username(name){
+    var par = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/;
+    var flag = par.test(name);
+    return flag;
+}
+
+function check_password(password){
+    var par = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/;
+    var flag = par.test(password);
+    return flag;
+}
+
+function check_email(email) {
+    var par = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+    var flag = par.test(email);
+    return flag;
+}
+
+window.onload=check_cookies();//切窗时触发事件
+
+function check_cookies(){
+    var name=get_cookie("username");
+    if(name!=""){
+        document.getElementById('logo_slogan').style.display='none';
+        document.getElementById('logo_slogan1').style.display='block';
+    }else{
+        document.getElementById('logo_slogan').style.display='block';
+        document.getElementById('logo_slogan1').style.display='block';
+    }
+}
+
+function get_cookies(name1){
+    var name=name1+"=";
+    var p=document.cookie.split(';');
+    for(var i=0;i<p.length;i++){
+        var c=ca[i].trim();//移除头尾空格
+        if(c.indexof(name)==0)
+            return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function check_username_now(){
+    var name=document.getElementById('username2').value;
+    if(name.length==0){
+        document.getElementById('name_label').innerHTML="<font color='red'>用户名不能为空</font>";
+    }else if(check_username(name)==false){
+        document.getElementById('name_label').innerHTML="<font color='red'>用户名格式错误</font>";
+    }else{
+        document.getElementById('name_label').innerHTML="<font color='red'></font>";
+    }
+}
+
+function check_password_now(){
+    var name=document.getElementById('password2').value;
+    if(name.length==0){
+        document.getElementById('password_label').innerHTML="<font color='red'>密码不能为空</font>";
+    }else if(check_password(name)==false){
+        document.getElementById('password_label').innerHTML="<font color='red'>密码格式错误</font>";
+    }else if(name==document.getElementById('username2')){
+        document.getElementById('password_label').innerHTML="<font color='red'>密码不能与用户名相同</font>";
+    }else{
+        document.getElementById('password_label').innerHTML="<font color='red'></font>";       
+    }
+}
+
+function check_password_now2(){
+    var p1=document.getElementById('password2').value;
+    var p2=document.getElementById('check_password').value;
+    if(p1!=p2){
+        document.getElementById('check_label').innerHTML="<font color='red'>密码不相同</font>";
+    }else{
+        document.getElementById('check_label').innerHTML="<font color='red'></font>";
+    }
+}
+
+function check_email_now(){
+    var email=document.getElementById('email').value;
+    if(email.length==0){
+        document.getElementById('email_label').innerHTML="<font color='red'>邮箱不能为空</font>";
+    }else if(check_email(email)==false){
+        document.getElementById('email_label').innerHTML="<font color='red'>邮箱格式错误</font>";
+    }else{
+        document.getElementById('email_label').innerHTML="<font color='red'></font>";
+    }
+}
